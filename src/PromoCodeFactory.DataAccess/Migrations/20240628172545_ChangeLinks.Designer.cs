425bbf1d6dc4e77f3db730e57fbe72f767756e13
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2024-06-28 22:26:48 +0500

    применяет миграцию

--- src/PromoCodeFactory.DataAccess/Migrations/20240628172545_ChangeLinks.Designer.cs
+++ src/PromoCodeFactory.DataAccess/Migrations/20240628172545_ChangeLinks.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PromoCodeFactory.DataAccess;
 
@@ -10,9 +11,11 @@ using PromoCodeFactory.DataAccess;
 namespace PromoCodeFactory.DataAccess.Migrations
 {
     [DbContext(typeof(DataBaseContext))]
-    partial class DataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240628172545_ChangeLinks")]
+    partial class ChangeLinks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
